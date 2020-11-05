4575d08dc3ea667591c6a1f66c94087e2bb771e5
Alexandr <alexvice02@users.noreply.github.com>
2020-11-05 20:43:15 +0200

    Додано час прибуття на станції

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-
+let prevEl = null
+let currentRoute = 0
 
 function showAllRoutes()
 {
@@ -16,20 +17,23 @@ function showRouteDetails(route_key)
     let routeDetailsDiv = $("#route-details")
 
     let markup = `
-        <h4 class="text-center">Маршрут ${route_key}</h4>
+        <h4>Маршрут ${route_key}</h4>
         <h6>Тип: автобус</h6>
         <br>
         <h6 class="text-center">Список зупинок</h6>
         <div class="list-group"></div>
     `
-    store.routes[route_key].waybill.forEach(item => {
-        markup += `<a href="#" class="list-group-item">${ store.stations[item[0]].name }</a>`
+
+    store.routes[route_key].waybill.forEach((item) => {
+        markup += `<a href="#" class="list-group-item">${ store.stations[item[0]].name }
+                    <span style="" class="badge badge-dark float-right">${item[1]}хв</span>
+                   </a>`
     })
 
 
     markup += `<h6 class="text-center mt-2">Час виїзду</h6>`
     store.routes[route_key].startTime.forEach((item) => {
-        markup += `<a href="#" class="badge badge-dark ml-2 route-departure-time">${item}</a>`
+        markup += `<a href="#" class="badge badge-primary ml-2 route-departure-time">${item}</a>`
     })
 
     routeDetailsDiv.html(markup)
@@ -38,11 +42,57 @@ function showRouteDetails(route_key)
 $("main").delegate(".route-link", "click", (event) => {
     event.preventDefault()
     showRouteDetails($(event.target).attr("data-route-key"))
+
+    currentRoute = $(event.target).attr("data-route-key")
+
+    if (prevEl !== null)
+    {
+        prevEl.removeClass("active")
+        prevEl = $(event.target)
+        $(event.target).toggleClass("active")
+    }else
+    {
+        prevEl = $(event.target)
+        $(event.target).toggleClass("active")
+    }
+
 })
+
+function fixTime(time, add)
+{
+    let hours = Number(time.split(":")[0])
+    let minutes = Number(time.split(":")[1])
+
+    if ((minutes + add) > 59)
+    {
+        minutes = (minutes + add) % 60;
+        hours++
+
+        return `${hours}:${minutes}`
+    }else
+    {
+        minutes = add + minutes;
+
+        return `${hours}:${minutes}`
+    }
+
+}
 
 $("main").delegate(".route-departure-time", "click", (event) => {
     event.preventDefault()
-    showRouteDetails($(event.target).attr("data-route-key"))
+
+    let markup = ''
+
+    let time = $(event.target).text()
+
+    store.routes[currentRoute].waybill.forEach((item, index) => {
+        markup += `<div class="list-group-item list-group-item-action">${store.stations[item[0]].name}
+            <span class="badge badge-dark float-right">${fixTime(time, item[1])}</span>
+        </div>`
+    })
+
+    $("#route-time-stations").html(markup)
+
 })
 
 showAllRoutes()
